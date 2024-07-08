6b9426b79c8f2826ffd878c0ed3fda2cbbfd1e36
Mboob <91790923+vic-mtf@users.noreply.github.com>
2024-07-08 21:21:02 +0200

    update

---
+++ src/components/Math.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { MathJaxContext, MathJax } from "better-react-mathjax";
+
+const config = {
+  loader: {
+    load: ["input/asciimath"],
+  },
+};
+
+function Mt(props) {
+  return (
+    <MathJaxContext config={config} src='/mathjax/es5/tex-chtml.js'>
+      <MathJax inline={true} hideUntilTypeset='first' dynamic={true}>
+        {props.children}
+      </MathJax>
+    </MathJaxContext>
+  );
+}
+
+export default React.memo(Mt);
